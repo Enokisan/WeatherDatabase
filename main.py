dbc298fe0ff6467c63aff7ed5d009edb82005363
Enokisan <eno.eno.business@gmail.com>
2021-06-13 01:21:42 +0900

    Add select area methods

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ import weatherMethods as we
 import tendl
 
 def main():
-    with open('tenki.json', 'r', encoding="UTF-8") as f:
-        data = json.load(f)
     while True:
+        with open('tenki.json', 'r', encoding="UTF-8") as f:
+            data = json.load(f)
         query_num = int(input("[Weather Database] What do you want? type {0:quit, 1:pick up all, 2:select area, 3: download json}\n"))
         if query_num == 0:
             print("[Weather Database] End. Bye-bye!\n")
             break
+
         elif query_num == 1:
             print("[Weather Database] Pick up all.\n")
             which_data = int(input("[Weather Database] type {0:back, 1:weathers, 2:winds, 3:waves}\n"))
@@ -17,18 +18,36 @@ def main():
                 print("[Weather Database] Back.\n")
                 continue
             elif which_data == 1:
-                we.weather_all(data)
+                we.weathers_all(data)
             elif which_data == 2:
                 we.winds_all(data)
             elif which_data == 3:
                 we.waves_all(data)
             else:
-                print("[Weather Database] {} is not supported!\n".format(which_data))
+                print("[Weather Database] Query {} is not supported!\n".format(which_data))
                 continue
+
+        elif query_num == 2:
+            area = input("[Weather Database] Select Area.\n")
+            which_data = int(input("[Weather Database] type {0:back, 1:weathers, 2:winds, 3:waves}\n"))
+            if which_data == 0:
+                print("[Weather Database] Back.\n")
+                continue
+            elif which_data == 1:
+                we.weather_sel(data, area)
+            elif which_data == 2:
+                we.wind_sel(data, area)
+            elif which_data == 3:
+                we.wave_sel(data, area)
+            else:
+                print("[Weather Database] Query {} is not supported!\n".format(which_data))
+                continue
+
         elif query_num == 3:
             tendl.download()
+
         else:
-            print("[Weather Database] {} is not supported!\n".format(query_num))
+            print("[Weather Database] Query {} is not supported!\n".format(query_num))
 
 if __name__ == "__main__":
     main()
--- weatherMethods.py
+++ weatherMethods.py
@@ -1,13 +1,13 @@
 import json
 
-def weather_all(data_in):
+def weathers_all(data_in):
     s = ""
     for area in data_in:
         name = area['name']
         s += "[ " + str(name) + " ]\n"
         for ts in area['srf']['timeSeries']:
             times = [n for n in ts['timeDefines']]
-            if 'winds' in ts['areas']:
+            if 'weathers' in ts['areas']:
                 for i,v in enumerate(ts['areas']['weathers']):
                     s += str(times[i]) + " : " + v + "\n"
     print(s)
@@ -31,11 +31,46 @@ def waves_all(data_in):
         s += "[ " + str(name) + " ]\n"
         for ts in area['srf']['timeSeries']:
             times = [n for n in ts['timeDefines']]
-            if 'winds' in ts['areas']:
+            if 'waves' in ts['areas']:
                 for i,v in enumerate(ts['areas']['waves']):
                     s += str(times[i]) + " : " + v + "\n"
     print(s)
 
+def weather_sel(data_in, area_name):
+    s = ""
+    for area in data_in:
+        if area['name'] == area_name:
+            s += "[ " + str(area_name) + " ]\n"
+            for ts in area['srf']['timeSeries']:
+                times = [n for n in ts['timeDefines']]
+                if 'weathers' in ts['areas']:
+                    for i,v in enumerate(ts['areas']['weathers']):
+                        s += str(times[i]) + " : " + v + "\n"
+    print(s)
+
+def wind_sel(data_in, area_name):
+    s = ""
+    for area in data_in:
+        if area['name'] == area_name:
+            s += "[ " + str(area_name) + " ]\n"
+            for ts in area['srf']['timeSeries']:
+                times = [n for n in ts['timeDefines']]
+                if 'winds' in ts['areas']:
+                    for i,v in enumerate(ts['areas']['winds']):
+                        s += str(times[i]) + " : " + v + "\n"
+    print(s)
+
+def wave_sel(data_in, area_name):
+    s = ""
+    for area in data_in:
+        if area['name'] == area_name:
+            s += "[ " + str(area_name) + " ]\n"
+            for ts in area['srf']['timeSeries']:
+                times = [n for n in ts['timeDefines']]
+                if 'waves' in ts['areas']:
+                    for i,v in enumerate(ts['areas']['waves']):
+                        s += str(times[i]) + " : " + v + "\n"
+    print(s)
 
 """
 # デバッグ用
